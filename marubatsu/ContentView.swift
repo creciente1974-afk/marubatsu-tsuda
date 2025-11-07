8046580f11ba3dcd427ed03bcf298fea62872781
creciente1974-afk <creciente1974@gmail.com>
2025-11-08 08:35:49 +0900

    Initial Commit

---
+++ marubatsu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  marubatsu
+//
+//  Created by tsuda kazumi on 2025/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ marubatsu/marubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  marubatsuApp.swift
+//  marubatsu
+//
+//  Created by tsuda kazumi on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct marubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
